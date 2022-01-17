c17191239fbe1ab4ab921d87617cefba1aff2a4d
Helton Quintãns <helton.quit@gmail.com>
2022-01-17 19:29:25 -0300

    refactoring function App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from "react";
-
+import React, {useEffect, useState} from "react";
+import api from "./services/api"
 import "./styles.css";
 
 function App() {
+
+  const [repository, setRepository] = useState([])
+  useEffect(()=>{
+    api.get('repositories').then(repository => {
+      {repository.data}
+    })
+  },[])
+
   async function handleAddRepository() {
     // TODO
   }
@@ -14,13 +22,15 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+          {repository.title}
 
           <button onClick={() => handleRemoveRepository(1)}>
             Remover
           </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
